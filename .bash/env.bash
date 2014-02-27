2f2a1c786a36025eaab10c9ffdfbb623eabd0d7b
Ben Scheirman <ben@scheirman.com>
2014-02-27 13:23:01 -0600

    Don't need these Ruby GC tweaks in Ruby 2.x

--- .bash/env.bash
+++ .bash/env.bash
@@ -5,10 +5,6 @@ export PATH=~/bin:~/.ec2/bin:/usr/local/bin:/usr/local/heroku/bin:/usr/local/sbi
 export EC2_HOME=~/.ec2
 export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Home
 
-# Rails Performance goodness
-export RUBY_GC_MALLOC_LIMIT=1000000000
-export RUBY_FREE_MIN=500000
-export RUBY_HEAP_MIN_SLOTS=40000
 
 # Load secrets into ENV if present
 secrets_path=~/.bash/secrets.bash
