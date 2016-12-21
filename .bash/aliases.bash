59045493faa51c30d3fc1dbfba90957001cafc1a
Ben Scheirman <ben@scheirman.com>
2016-12-21 11:22:59 -0600

    open project in stable Xcode, not beta

--- .bash/aliases.bash
+++ .bash/aliases.bash
@@ -122,6 +122,6 @@ function xc() {
     echo "Couldn't find a workspace or a project to open."
   else
     echo "Opening $project_file..."
-    open $project_file
+    open $project_file -a /Applications/Xcode.app
   fi
 }
