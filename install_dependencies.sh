c6ac72d1bb788cf58b1e99b67b9f68fc79a4c13f
Ben Scheirman <ben@scheirman.com>
2021-03-21 07:45:04 -0500

    Make sure nvim is symlinked

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -4,6 +4,18 @@ set -e
 
 # This will install dependencies used by these dotfiles. Assumes homebrew is installed.
 
+echo "📦 Installing neovim..."
 brew install neovim
+
+echo "📦 Installing git-delta..."
 brew install git-delta
+
+# Symlink vim -> nvim
+if [[ "$(readlink /usr/local/bin/vim)" == "/usr/local/bin/nvim" ]]
+then
+  echo "✅ vim is already symlinked to nvim"
+else
+  echo "⚙️  symlink vim → nvim"
+  ln -s /usr/local/bin/{n,}vim
+fi
 
