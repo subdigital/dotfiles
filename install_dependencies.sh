d3365b5497001b33ed1391fc4c3a9d3b60cefa1e
Ben Scheirman <ben@scheirman.com>
2021-03-21 10:15:48 -0500

    Install ag, wget, hub, and a powerline font

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -10,6 +10,15 @@ brew install neovim
 echo "📦 Installing git-delta..."
 brew install git-delta
 
+echo "📦 Installing ag..."
+brew install ag
+
+echo "📦 Installing wget..."
+brew install wget
+
+echo "📦 Installing hub..."
+brew install hub
+
 # Symlink vim -> nvim
 if [[ "$(readlink /usr/local/bin/vim)" == "/usr/local/bin/nvim" ]]
 then
@@ -17,5 +26,14 @@ then
 else
   echo "⚙️  symlink vim → nvim"
   ln -s /usr/local/bin/{n,}vim
+fi
+
+# Fonts
+if [[ ! -f ~/Library/Fonts/DejaVu%20Sans%20Mono%20for%20Powerline.ttf ]]
+then
+  echo "Installing DejaVu Sans Mono (Powerline) font..."
+  cd ~/Library/Fonts && \
+    wget https://github.com/powerline/fonts/blob/master/DejaVuSansMono/DejaVu%20Sans%20Mono%20for%20Powerline.ttf && \
+    cd -
 fi
 
