f2b7dd26a20003fb70ed80dc6153b4720158d5b8
Ben Scheirman <ben@scheirman.com>
2024-02-08 14:03:48 -0600

    Add/configure harpoon

--- .config/nvim/lua/ben/plugins/harpoon.lua
+++ .config/nvim/lua/ben/plugins/harpoon.lua
@@ -9,10 +9,13 @@ return {
 
     local keymap = vim.keymap
 
-    keymap.set("n", "<leader>hm", "<cmd>lua require('harpoon.mark').add_file()<cr>", { desc = "Mark file with harpoon" })
+    keymap.set("n", "<leader>ha", "<cmd>lua require('harpoon.mark').add_file()<cr>", { desc = "Add file with harpoon" })
     keymap.set("n", "<leader>hn", "<cmd>lua require('harpoon.ui').nav_next()<cr>", { desc = "Go to next harpoon mark" })
     keymap.set("n", "<leader>hp", "<cmd>lua require('harpoon.ui').nav_prev()<cr>", { desc = "Go to previous harpoon mark" })
-    keymap.set("n", "<leader>hh", "<cmd>Telescope harpoon marks<cr>", { desc = "Show harpoon marks" })
-
+    keymap.set("n", "<leader>ht", "<cmd>Telescope harpoon marks<cr>", { desc = "Search harpoon marks" })
+    keymap.set("n", "<leader>hh", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", { desc = "Toggle harpoon UI" })
+    keymap.set("n", "<leader>1", "<cmd>lua require('harpoon.ui').nav_file(1)<cr>", { desc = "Navigate to harpoon 1" })
+    keymap.set("n", "<leader>2", "<cmd>lua require('harpoon.ui').nav_file(2)<cr>", { desc = "Navigate to harpoon 2" })
+    keymap.set("n", "<leader>3", "<cmd>lua require('harpoon.ui').nav_file(3)<cr>", { desc = "Navigate to harpoon 3" })
   end
 }
