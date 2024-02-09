b03aac29e61ccbced0ce36b78a891d8958470855
Ben Scheirman <ben@scheirman.com>
2024-02-09 13:30:48 -0600

    Add catppuccin theme

--- .config/nvim/lua/ben/plugins/colorschemes.lua
+++ .config/nvim/lua/ben/plugins/colorschemes.lua
@@ -23,7 +23,18 @@ return {
     priority = 1000,
     opts = {},
     config = function()
-      vim.cmd([[colorscheme moonfly]])
+      -- vim.cmd([[colorscheme moonfly]])
+    end
+  },
+  {
+    "catppuccin/vim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd([[colorscheme catppuccin_mocha]])
+      -- vim.cmd([[colorscheme catppuccin_latte]])
+      -- vim.cmd([[colorscheme catppuccin_frappe]])
+      -- vim.cmd([[colorscheme catppuccin_macchiato]])
     end
   }
 }
