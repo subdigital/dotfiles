ef3e0c18a94b5154e2b07e2635dcc449d254be55
Ben Scheirman <ben@scheirman.com>
2024-02-13 09:47:50 -0600

    Useful keymaps for moving text, indenting

--- .config/nvim/lua/ben/core/keymaps.lua
+++ .config/nvim/lua/ben/core/keymaps.lua
@@ -21,3 +21,32 @@ km.set("n", "<leader>V", ":tabe ~/.config/nvim/init.lua<cr>:Lazy load neo-tree.n
 
 -- close buffer
 km.set("n", "<leader>bc", "<cmd>bdelete<cr>", { noremap = true, silent = true })
+
+-- easier saving/quitting
+km.set("n", "<leader>w", "<cmd>w<cr>", { noremap = true, silent = true, desc = "Save"})
+km.set("n", "<leader>q", "<cmd>q<cr>", { noremap = true, silent = true, desc = "Quit"})
+km.set("n", "<leader>wq", "<cmd>wq<cr>", { noremap = true, silent = true, desc = "Quit"})
+
+-- find/replace in current buffer (for project wide, look in spectre.lua)
+km.set("n", "S", function()
+  local cmd = ":%s/<c-r><c-w>/<c-r><c-w>/gI<Left><Left><Left>"
+  local keys = vim.api.nvim_replace_termcodes(cmd, true, false, true)
+  vim.api.nvim_feedkeys(keys, "n", false)
+end)
+
+-- center buffer while vertical scrolling
+km.set("n", "<c-u>", "<c-u>zz", { silent = true, noremap = true })
+km.set("n", "<c-d>", "<c-d>zz", { silent = true, noremap = true })
+km.set("n", "G", "Gzz", { silent = true, noremap = true })
+
+-- move selection up and down
+km.set("x", "<a-j>", ":m '>+1<cr>gv=gv", { silent = true, noremap = true })
+km.set("x", "<a-k>", ":m '<-2<cr>gv=gv", { silent = true, noremap = true })
+
+-- move line up and down
+km.set("n", "<a-k>", ":m .-2<cr>==", { silent = true, noremap = true })
+km.set("n", "<a-j>", ":m .+1<cr>==", { silent = true, noremap = true })
+
+-- keep selection while indenting
+km.set("v", ">", ">gv", { silent = true, noremap = true })
+km.set("v", "<", "<gv", { silent = true, noremap = true })
