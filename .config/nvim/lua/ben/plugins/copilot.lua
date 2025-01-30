4319c78113d8277c75868e6438379fb3ed048410
Ben Scheirman <ben@scheirman.com>
2025-01-30 10:08:48 -0600

    Copilot setup with dependencies

--- .config/nvim/lua/ben/plugins/copilot.lua
+++ .config/nvim/lua/ben/plugins/copilot.lua
@@ -1,8 +1,13 @@
 return {
   "zbirenbaum/copilot-cmp",
+  dependencies = {
+    "zbirenbaum/copilot.lua"
+  },
   enabled = true,
   event = { "BufEnter" },
   config = function()
+    require("copilot").setup({})
+
     require("copilot_cmp").setup({
       suggestion = { enabled = true },
       panel = { enabled = true }, -- false will disable the suggestion popu
