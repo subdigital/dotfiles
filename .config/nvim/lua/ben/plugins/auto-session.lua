21bb4d5ba6fe43b5128230a7d969baa222e3e0a3
Ben Scheirman <ben@scheirman.com>
2025-01-30 10:08:14 -0600

    Remove auto-sessions, this was more pain that it was worth

--- .config/nvim/lua/ben/plugins/auto-session.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'rmagatti/auto-session',
-  config = function()
-    require("auto-session").setup {
-      log_level = "error",
-      auto_session_suppress_dirs = { "~/", "~/dev", "~/nikedev", "~/Downloads", "/"},
-    }
-  end
-}
