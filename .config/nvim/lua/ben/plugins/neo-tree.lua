25d9905a266624fedc28cb4a3d7217a7fc24c465
Ben Scheirman <ben@scheirman.com>
2025-01-30 10:15:05 -0600

    Y to copy file, path, etc from tree

--- .config/nvim/lua/ben/plugins/neo-tree.lua
+++ .config/nvim/lua/ben/plugins/neo-tree.lua
@@ -1,5 +1,6 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
+  lazy = false,
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-web-devicons",
@@ -9,7 +10,12 @@ return {
   },
   keys = {
     { "<leader>e", "<cmd>Neotree toggle<cr>", desc = "NeoTree", silent = true },
-    { "<leader>j", "<cmd>Neotree filesystem reveal left reveal_file=%<cr>", desc = "Reveal file NeoTree", silent = true },
+    {
+      "<leader>j",
+      "<cmd>Neotree filesystem reveal left reveal_file=%<cr>",
+      desc = "Reveal file NeoTree",
+      silent = true,
+    },
   },
   opts = {
     filesystem = {
@@ -24,6 +30,42 @@ return {
     }
   },
   config = function()
-    require("neo-tree").setup()
-  end
+    require("neo-tree").setup({
+      window = {
+        mappings = {
+          ["Y"] = function(state)
+            -- NeoTree is based on [NuiTree](https://github.com/MunifTanjim/nui.nvim/tree/main/lua/nui/tree)
+            -- The node is based on [NuiNode](https://github.com/MunifTanjim/nui.nvim/tree/main/lua/nui/tree#nuitreenode)
+            local node = state.tree:get_node()
+            local filepath = node:get_id()
+            local filename = node.name
+            local modify = vim.fn.fnamemodify
+
+            local results = {
+              filepath,
+              modify(filepath, ":."),
+              modify(filepath, ":~"),
+              filename,
+              modify(filename, ":r"),
+              modify(filename, ":e"),
+            }
+
+            vim.ui.select({
+              "1. Absolute path: " .. results[1],
+              "2. Path relative to CWD: " .. results[2],
+              "3. Path relative to HOME: " .. results[3],
+              "4. Filename: " .. results[4],
+              "5. Filename without extension: " .. results[5],
+              "6. Extension of the filename: " .. results[6],
+            }, { prompt = "Choose to copy to clipboard:" }, function(choice)
+              local i = tonumber(choice:sub(1, 1))
+              local result = results[i]
+              vim.fn.setreg("*", result)
+              vim.notify("Copied: " .. result)
+            end)
+          end,
+        },
+      },
+    })
+  end,
 }
