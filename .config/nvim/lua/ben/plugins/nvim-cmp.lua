c7720c3271934478cde8fee87d5a2d071a122762
Ben Scheirman <ben@scheirman.com>
2025-01-30 10:12:15 -0600

    Add copilot to completions (wip tab behavior)

--- .config/nvim/lua/ben/plugins/nvim-cmp.lua
+++ .config/nvim/lua/ben/plugins/nvim-cmp.lua
@@ -18,33 +18,32 @@ return {
     cmp.setup({
 
       completion = {
-        completeopt = "menu,menuone,preview,noselect"
+        completeopt = "menu,menuone,preview,noselect",
       },
 
       snippet = {
         expand = function(args)
           luasnip.lsp_expand(args.body)
-        end
+        end,
       },
 
       mapping = cmp.mapping.preset.insert({
-        ["<S-tab>"] = cmp.mapping.select_prev_item(),
-        ["<tab>"] = cmp.mapping.select_next_item(),
+        -- ["<S-tab>"] = cmp.mapping.select_prev_item(),
+        -- ["<tab>"] = cmp.mapping.select_next_item(),
         ["<C-b>"] = cmp.mapping.scroll_docs(-4),
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
         ["<C-space>"] = cmp.mapping.complete(), -- show completion suggestions
         ["<c-e>"] = cmp.mapping.abort(), -- close completion window
-        ["<CR>"] = cmp.mapping.confirm({ select = false}),
+        ["<CR>"] = cmp.mapping.confirm({ select = false }),
       }),
 
       sources = cmp.config.sources({
         { name = "nvim_lsp" },
         { name = "luasnip" },
+        { name = "copilot" },
         { name = "buffer" },
         { name = "path" },
       }),
-
     })
-  end
+  end,
 }
-
