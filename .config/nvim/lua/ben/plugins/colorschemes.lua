1ad1fe537456f00d88b27f89f366279f4dea1952
Ben Scheirman <ben@scheirman.com>
2025-01-30 10:08:33 -0600

    ADD with colorschemes, currently nightfly

--- .config/nvim/lua/ben/plugins/colorschemes.lua
+++ .config/nvim/lua/ben/plugins/colorschemes.lua
@@ -5,7 +5,7 @@ return {
     priority = 1000,
     opts = {},
     config = function()
-      --vim.cmd([[colorscheme tokyonight]])
+      -- vim.cmd([[colorscheme tokyonight]])
     end
   },
   {
@@ -14,7 +14,7 @@ return {
     priority = 1000,
     opts = {},
     config = function()
-      -- vim.cmd([[colorscheme nightfly]])
+      vim.cmd([[colorscheme nightfly]])
     end
   },
   {
@@ -32,10 +32,22 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-      vim.cmd([[colorscheme catppuccin-mocha]])
+      -- vim.cmd([[colorscheme catppuccin-mocha]])
       -- vim.cmd([[colorscheme catppuccin-latte]])
       -- vim.cmd([[colorscheme catppuccin-frappe]])
       -- vim.cmd([[colorscheme catppuccin-macchiato]])
     end
+  },
+  {
+    "xiyaowong/transparent.nvim",
+    lazy = false,
+    enabled = false,
+    priority = 1000,
+    config = function()
+      require("transparent").setup({
+        exclude_groups = { "NotifyBackground" }
+      })
+    end
   }
 }
+
