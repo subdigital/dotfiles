243d5c8b78d845b4fc96a1e3c452b19213deb7af
Ben Scheirman <ben@scheirman.com>
2025-02-01 11:08:20 -0600

    Update lazy/lock

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,7 +1,17 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
 require("ben.core")
 require("ben.lazy")
-
-if vim.g.neodvide then
-  require("ben.neovide")
-end
 
