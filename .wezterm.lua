f434bf0c4d9fc41f5d5af203e3b10d8d4efdbff2
Ben Scheirman <ben@scheirman.com>
2025-02-01 11:06:06 -0600

    Wezterm stuff
    Not using this currently, but keeping it around for now

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,18 +1,17 @@
 local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
-local act = wezterm.action
 
--- config.color_scheme = "AdventureTime"
+require("tabs").setup(config)
+require("keys").setup(config)
+
 -- config.color_scheme = "Batman"
 -- config.color_scheme = "Tango (base16)"
-config.color_scheme = "Thayer Bright"
+-- config.color_scheme = "Thayer Bright"
+config.color_scheme = "tokyonight_night"
 config.font = wezterm.font "Iosevka Nerd Font"
 config.font_size = 18
 config.line_height = 1.1
-
--- tabs
-config.hide_tab_bar_if_only_one_tab = true
 
 -- window
 config.window_padding = {
@@ -108,4 +107,3 @@ config.keys = {
 }
 
 return config
-
