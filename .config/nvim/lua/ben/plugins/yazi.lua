54da96ca108d8e423df84836ab0096c8b2e7b18b
Ben Scheirman <ben@scheirman.com>
2025-02-10 08:52:15 -0600

    Add yazi.nvim

---
+++ .config/nvim/lua/ben/plugins/yazi.lua
@@ -0,0 +1,22 @@
+return {
+  "mikavilpas/yazi.nvim",
+  keys = {
+    {
+      "<leader>-",
+      mode = { "n", "v" },
+      "<cmd>Yazi<cr>",
+      desc = "Open yazi at the current file"
+    },
+    {
+      "<leader>cw",
+      mode = { "n", "v" },
+      "<cmd>Yazi cwd<cr>",
+      desc = "Open yazi at the current working directory"
+    }
+  },
+  opts = {
+    keymaps = {
+      change_working_directory = "."
+    }
+  },
+}
