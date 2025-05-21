cdac47fe5310439b2f852c6f00bd8b94e433d87e
Ben Scheirman <ben@scheirman.com>
2025-05-21 07:27:57 -0500

    Add diagnostics buffer config to trouble

--- .config/nvim/lua/ben/plugins/trouble.lua
+++ .config/nvim/lua/ben/plugins/trouble.lua
@@ -4,14 +4,26 @@ return {
     "nvim-tree/nvim-web-devicons"
   },
   keys = {
-    { "<leader>xx", function() require("trouble").toggle() end, desc = "Trouble toggle" },
-    { "<leader>xw", function() require("trouble").toggle("workspace_diagnostics") end, desc = "Workspace diagnostics" },
-    { "<leader>xb", function() require("trouble").toggle("document_diagnostics") end, desc = "Buffer diagnostics" },
-    { "<leader>xq", function() require("trouble").toggle("quickfix") end, desc = "Quickfix" },
-    { "<leader>xl", function() require("trouble").toggle("loclist") end, desc = "loclist" },
-    { "<leader>xr", function() require("trouble").toggle("lsp_references") end, desc = "LSP References" },
+    {
+      "<leader>xx",
+      "<cmd>Trouble diagnostics toggle<cr>",
+      desc = "Trouble diagnostics"
+    },
+    {
+      "<leader>xb",
+      "<cmd>Trouble diagnostics_buffer toggle<cr>",
+      desc = "Buffer diagnostics"
+    }
+    -- { "<leader>xq", function() require("trouble").toggle("quickfix") end, desc = "Quickfix" },
+    -- { "<leader>xl", function() require("trouble").toggle("loclist") end, desc = "loclist" },
+    -- { "<leader>xr", function() require("trouble").toggle("lsp_references") end, desc = "LSP References" },
   },
   opts = {
-
+    modes = {
+      diagnostics_buffer = {
+        mode = "diagnostics",
+        filter = { buf = 0 }
+      }
+    }
   }
 }
