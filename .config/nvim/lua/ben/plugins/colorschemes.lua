acc7ed95ebe09db4359d6e8f7f92245d4aa34759
Ben Scheirman <ben@scheirman.com>
2025-05-31 11:16:44 -0500

    colorschemes

--- .config/nvim/lua/ben/plugins/colorschemes.lua
+++ .config/nvim/lua/ben/plugins/colorschemes.lua
@@ -7,10 +7,10 @@ return {
         transparent = true,
         theme = "dragon",
       })
-      vim.cmd([[colorscheme kanagawa]])
+      --vim.cmd([[colorscheme kanagawa]])
     end,
     build = function()
-      vim.cmd("KanagawaCompile")
+      -- vim.cmd("KanagawaCompile")
     end,
   },
   {
@@ -61,6 +61,49 @@ return {
       require("transparent").setup({
         exclude_groups = { "NotifyBackground" },
       })
+    end,
+  },
+  {
+    "lunacookies/vim-colors-xcode",
+    config = function() end,
+  },
+
+  {
+    "https://github.com/RRethy/base16-nvim",
+    config = function()
+      local bg = "#0F1919"
+      local accent = "#102121"
+      local accent2 = "#0D2525" -- highlight
+
+      local text = "#abb2bf"
+      local dark_text = "#3E4451" -- comments, line numbers
+
+      local keyword = "#8F939A"
+      local func = "#B6AB8B"
+      local types = "#65838E"
+      local constant = "#A06057"
+
+      local for_tesing = "#FF0000"
+
+      require("base16-colorscheme").setup({
+        base00 = bg,
+        base01 = accent,
+        base02 = accent2,
+        base03 = dark_text,
+        base04 = dark_text,
+        base05 = text,
+        base06 = for_tesing,
+        base07 = for_tesing,
+        base08 = text,
+        base09 = constant,
+        base0A = types,
+        base0B = constant,
+        base0C = text,
+        base0D = func,
+        base0E = keyword,
+        base0F = text,
+      })
+      -- vim.cmd([[colorscheme base16]])
     end,
   },
 }
